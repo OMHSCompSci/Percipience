77e8eadbe65931a97081e70bb830a919681e58cd
Morgan <Morgan@MORGAN-LAPTOP.home>
2016-11-30 17:48:03 -0500

    Players now move with moving blocks

--- CompSciClubGame/src/omhscsc/world/MovingBox.java
+++ CompSciClubGame/src/omhscsc/world/MovingBox.java
@@ -5,21 +5,27 @@ import java.awt.Graphics;
 
 import omhscsc.state.GameStateState;
 import omhscsc.util.Hitbox;
+import omhscsc.util.Velocity;
 
 public class MovingBox extends WorldObject { //Moving block
-	private double vx,vy; //velocities in x and y direction
+	private Velocity v; //velocities in x and y direction
 	
 	public MovingBox(World world, int x, int y, int w, int h, Color c, double vx, double vy) {
 		super(world,x,y,w,h,c);
-		this.vx = vx;
-		this.vy = vy;
+		this.v = new Velocity(vx,vy);
 	}
 	@Override
 	public void tick(GameStateState s) { //Changes position of hitbox to hitbox+velocity
 		Hitbox h = super.getHitbox();
-		h.addX(vx);
-		h.addY(vy);
+		h.addX(v.getX());
+		h.addY(v.getY());
 		super.changeHitbox(h);
+		Hitbox t = new Hitbox(h.getBounds().width,h.getBounds().height,h.getLocation());
+		t.addY(-1);
+		if (s.getPlayer().getHitbox().getBounds().intersects(t.getBounds())) {//player is touching box
+			s.getPlayer().getHitbox().addX(v.getX());
+			s.getPlayer().getHitbox().addX(v.getY());
+		}
 	}
 	
 	@Override
