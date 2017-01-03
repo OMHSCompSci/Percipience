554af1af169f80aed05f5776818ce0d0112b8344
kngo0784 <kngo0784@OMHB45C4289E31.inst.hcpss.org>
2017-01-03 13:39:51 -0500

    Tips and trix

--- CompSciClubGame/src/omhscsc/world/designer/WorldDesignerGUI.java
+++ CompSciClubGame/src/omhscsc/world/designer/WorldDesignerGUI.java
@@ -1,5 +1,32 @@
 package omhscsc.world.designer;
 
-public class WorldDesignerGUI {
+public class WorldDesignerGUI  {
+	
+	/*
+	 * To do:
+	 * Create JFrame
+	 * render edited world to jframe directly
+	 * allow zoom in / out
+	 * be able to drag and drop the different types of world objects into the world, and be able to enter width and height and x and y values manually
+	 * be able to create rectangles with the mouse to create WorldRegion's.
+	 * be able to move camera location around too
+	 * 
+	 * Available resources to look at and use:
+	 * World.java
+	 * Camera.java
+	 * WorldObject.java (and its sub classes)
+	 * Anchor.java
+	 * Hitbox.java
+	 * Location.java
+	 * RenderableGameObject.java
+	 * GameObject.java
+	 * Game.java (look at the window creation and stuff)
+	 * 
+	 * Also look up:
+	 * JFrame documentation (google)
+	 * MouseAdapter documentation (google)
+	 * KeyAdapter documentation (google)
+	 * 
+	 */
 
 }
