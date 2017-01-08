3bbfb6a66690ff10fdfb20a45706e314aaf46807
xDest | Zach <destroyer7712@gmail.com>
2017-01-08 15:57:40 -0500

    Small work on PathBox

--- CompSciClubGame/src/omhscsc/world/PathBox.java
+++ CompSciClubGame/src/omhscsc/world/PathBox.java
@@ -3,14 +3,19 @@ package omhscsc.world;
 import java.awt.Color;
 import java.util.List;
 
+import omhscsc.Game;
 import omhscsc.state.GameStateState;
+import omhscsc.util.Hitbox;
 import omhscsc.util.Location;
+import omhscsc.util.Velocity;
 
 public class PathBox extends WorldObject {
 
 	protected List<Location>path;
+	private Velocity currentVelocity;
 	protected int current,next,last;
 	protected double speed;
+	protected final float error = 1f;
 	/**
 	 * PathBox is similar to {@link MovingBox}, except it stays on a specific path and moves up and down it's list of points.
 	 * @param world The world it's in
@@ -37,9 +42,43 @@ public class PathBox extends WorldObject {
 		 * Have the Box move from the current point to the next point, also make sure
 		 * it doesn't overshoot. After it reaches one point, switch to the next and change "current, next, and last"
 		 */
-		//Too lazy to do this right now lol
+		if(currentVelocity.getX() == 0 && currentVelocity.getY() == 0)
+			findVelocity();
+		Hitbox h = super.getHitbox();
+		h.addX((currentVelocity.getX()/Game.TPS)* Game.getTimeRate());
+		h.addY((currentVelocity.getY()/Game.TPS)* Game.getTimeRate());
+		super.changeHitbox(h);
+		Hitbox t = new Hitbox(h.getBounds().width,h.getBounds().height,h.getLocation());
+		t.addY(-1); //covers bottom pixel of player
+		if (s.getPlayer().getHitbox().getBounds().intersects(t.getBounds())) {//player is touching box
+			s.getPlayer().getHitbox().addX((currentVelocity.getX()/Game.TPS) * Game.getTimeRate());
+			s.getPlayer().getHitbox().addY((currentVelocity.getY()/Game.TPS) * Game.getTimeRate());
+		}
+	}
+	
+	private void findVelocity() {
+		//Cannot exceed speed.
+		double xv = 0;
+		double yv = 0;
+		double xdist = (path.get(current).getX() - this.getHitbox().getLocation().getX());
+		double ydist = (path.get(current).getY() - this.getHitbox().getLocation().getY());
+		double angle = Math.atan(ydist/xdist);
+		//This might not work for everything
+		//Not finished
 	}
 
+	private void toNext() {
+		last = current;
+		current = next;
+		next = getNext();
+	}
+	
+	public int getNext() {
+		int c = current+1;
+		if(c >= path.size())
+			return 0;
+		return c;
+	}
 	
 	
 }
